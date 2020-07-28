1e41797d7ee6a151dcdf00257bc3f45b3b96f856
lairias <lairias@unah.hn>
2020-07-28 16:00:07 -0600

    configuracion de las Bariables de entorno

--- server/config/database.js
+++ server/config/database.js
@@ -1,18 +1,23 @@
 //configuracion de la base de datos
 const Sequelize = require('sequelize')
-
-module.exports = new Sequelize("agencia de viajes", "root", "", {
-  host: "127.0.0.1",
-  ports: "3306",
-  dialect:'mysql',
-  define: {
-      timestamps: false
-  },
-  poll:{
-      max:5,
-      min:0,
-      acquire:30000,
-      idle: 10000
-  },
-  operatorsAliases: false
-});
+require('dotenv').config({path:'variables.env'})
+module.exports = new Sequelize(
+  process.env.BD_NOMBRE,
+  process.env.BD_USER,
+  process.env.BD_PASS,
+  {
+    host: process.env.BD_HOST,
+    ports: process.env.BD_PORT,
+    dialect: "mysql",
+    define: {
+      timestamps: false,
+    },
+    poll: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+    operatorsAliases: false,
+  }
+);
--- server/index.js
+++ server/index.js
@@ -9,9 +9,13 @@ const Routes = require('./routes');
 const app = express()
 
 //importamos la configuracion de la base de datos
-// const db = require('./config/database.js')
-// db.authenticate()
-//     .then(()=> console.log('Conactado a la base de datos'))
+const db = require('./config/database.js')
+
+require('dotenv').config({path:'variables.env'})
+
+
+db.authenticate()
+    .then(()=> console.log('Conactado a la base de datos'))
 //     .catch(error => console.log(error))
 
 //----------------------setting Template Engine 
@@ -31,8 +35,11 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended:true}))
 app.use('/',Routes())
 
+//puerto host de la app
 
-
-
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 3000;
 //-----------------------Section del  PORT
-app.listen(3000);
+app.listen(port, host,()=>{
+    console.log('El servidor conectado')
+});
